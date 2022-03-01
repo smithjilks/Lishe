2bce170c776e922d5b10debdf8db4f8d90108e0c
smithjilks <smithjilks@gmail.com>
2022-03-01 16:29:50 +0300

    Fix(api auth): Fix semistandard errors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require("path");
+const path = require('path');
 
 const mongoose = require('mongoose');
 
@@ -7,23 +7,20 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://' + process.env.MONGO_ATLAS_USER + ':' + process.env.MONGO_ATLAS_PW + '@lishe-primary.kttbi.mongodb.net/lisheTest?retryWrites=true&w=majority', {useNewUrlParser: true,  useUnifiedTopology: true } )
-.then( () => {
-  console.log('Connected to Database');
-})
-.catch( ()=> {
-  console.log('Connection failed');
-});
-
+mongoose.connect('mongodb+srv://' + process.env.MONGO_ATLAS_USER + ':' + process.env.MONGO_ATLAS_PW + '@lishe-primary.kttbi.mongodb.net/lisheTest?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to Database');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Grant access to the images folder
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-
-app.use("/api/v1/users", userRoutes);
+app.use('/api/v1/users', userRoutes);
 
 module.exports = app;
-
