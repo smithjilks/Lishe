bee1c42f40e8b09398cb208e47fb956f9787e5e5
smithjilks <smithjilks@gmail.com>
2022-03-09 01:58:39 +0300

    Feat(android intro nav): Create navigation to login and signup

--- android/app/src/main/java/com/smith/lishe/IntroActivity.kt
+++ android/app/src/main/java/com/smith/lishe/IntroActivity.kt
@@ -1,11 +1,31 @@
 package com.smith.lishe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.smith.lishe.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.introLoginButton.setOnClickListener { navToLoginActivity() }
+        binding.introSignupButton.setOnClickListener { navToSignupActivity() }
+
+
+    }
+
+    private fun navToSignupActivity() {
+        val intent = Intent(this, SignupActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
