ddd415cfd15c52555c331f50b6e890baaa16d5e7
smithjilks <smithjilks@gmail.com>
2022-03-08 03:48:23 +0300

    Feat(android ui): Add food details activity

---
+++ android/app/src/main/java/com/smith/lishe/FoodListingDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.smith.lishe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodListingDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_listing_details)
+    }
+}
